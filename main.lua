55e26397910cc4ba42be3b3d94e50104ab5e86ef
Nolan Gormley <nolan@chode.nolangormley.com>
2017-11-17 00:38:47 -0500

    something I guess

--- main.lua
+++ main.lua
@@ -1,30 +1,76 @@
 function love.load()
-	x = 100
-	y = 50
-	vel = 500
-	reverseX = false
-	reverseY = false
+-- our tiles
+	tile = {}
+	for i=0,1 do -- change 3 to the number of tile images minus 1.
+		tile[i] = love.graphics.newImage( "tile"..i..".png" )
+	end
+
+	love.graphics.setNewFont(12)
+
+	-- map variables
+	map_w = 20
+	map_h = 20
+	map_x = 0
+	map_y = 0
+	map_offset_x = 30
+	map_offset_y = 30
+	map_display_w = 10
+	map_display_h = 10
+	tile_w = 40
+	tile_h = 40
+	map={
+	   { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}
 end
 
-function love.update(dt)
-	if love.keyboard.isDown("up")  or
-		love.keyboard.isDown("w") then
-		y = y - vel * dt
-	elseif love.keyboard.isDown("down")  or
-		love.keyboard.isDown("s") then
-		y = y + vel * dt
-	end
+function draw_map()
+   for y=1, map_display_h do
+      for x=1, map_display_w do                                                         
+         love.graphics.draw( 
+            tile[map[y+map_y][x+map_x]], 
+            (x*tile_w)+map_offset_x, 
+            (y*tile_h)+map_offset_y )
+      end
+   end
+end
 
-	if love.keyboard.isDown("right")  or
-		love.keyboard.isDown("d") then
-		x = x + vel * dt
-	elseif love.keyboard.isDown("left")  or
-		love.keyboard.isDown("a") then
-		x = x - vel * dt
+function love.keypressed(key, unicode)
+	if key == 'up' then
+		map_y = map_y-1
+		if map_y < 0 then map_y = 0; end
 	end
-
+	if key == 'down' then
+		map_y = map_y+1
+		if map_y > map_h-map_display_h then map_y = map_h-map_display_h; end
+	end
+ 
+	if key == 'left' then
+		map_x = math.max(map_x-1, 0)
+	end
+	if key == 'right' then
+		map_x = math.min(map_x+1, map_w-map_display_w)
+	end
 end
 
 function love.draw()
-    love.graphics.circle("fill", x, y, 20)
+	draw_map()
 end
